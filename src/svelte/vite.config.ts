17d45fd5bbc20d9a66d0e85472f45f31e8b6647b
Ben Beckford <ben@benjaminbeckford.com>
2026-02-01 12:20:28 -0800

    Fix web controller base path

--- src/svelte/vite.config.ts
+++ src/svelte/vite.config.ts
@@ -3,5 +3,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
-  base: "http://arduino.local/",
+  base: "https://benbeckford.github.io/COGS300/",
 });
