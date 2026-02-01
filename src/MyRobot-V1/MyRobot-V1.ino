78ae6f89047b02f3078a0357ac83591d8f522157
Ben Beckford <ben@benjaminbeckford.com>
2026-01-31 22:04:58 -0800

    Enable WebSocket control

--- src/MyRobot-V1/MyRobot-V1.ino
+++ src/MyRobot-V1/MyRobot-V1.ino
@@ -1,26 +1,76 @@
-// MyRobot-V1.ino
-// This file must be named the same as your sketch folder
-int enA = 9;   // Enable pin for Motor A — must be a PWM-capable pin
-int in1 = 8;   // Direction control pin 1 for Motor A
-int in2 = 7;   // Direction control pin 2 for Motor A
+#include "WiFiS3.h"
+#include <WebSocketsServer.h>
+
+const int enA = 9;
+const int in1 = 8;
+const int in2 = 7;
+const int enB = 10;
+const int in3 = 6;
+const int in4 = 5;
+
+IPAddress ip(192, 168, 4, 1);
+WiFiServer server(80);
+WebSocketsServer webSocket(81);
+
 
 void setup() {
-    // put your setup code here, to run once:
     Serial.begin(9600);
+
+    pinMode(enA, OUTPUT);
+    pinMode(enB, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+
+    WiFi.config(ip);
+    WiFi.beginAP("COGS 300 Robot", "");
+
+    delay(5000);
+
+    server.begin();
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+    if (type == WStype_BIN && length == 2) {
+        uint8_t motor = *payload & 0xF0;
+        uint8_t dir = *payload & 0xF;
+
+        digitalWrite(motor ? in3 : in1, dir ? HIGH : LOW);
+        digitalWrite(motor ? in4 : in2, dir ? LOW : HIGH);
+        analogWrite(motor ? enB : enA, payload[1]);
+    }
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    webSocket.loop();
 
-    // Drive for 1 second
-    logInfo(Serial, "Driving");
-    drive(in1, in2, enA);
-    delay(1000);
+    WiFiClient client = server.available();
 
-    // Stop for 1 second
-    logInfo(Serial, "Stopping");
-    stop(in1, in2, enA);
-    delay(1000);
+    if (client) {
+        char last = 0;
 
-    // TODO: add your own driving functions here
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+
+                if ((c == '\r' || c == '\n') && (last == '\r' || last == '\n')) {
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println();
+
+                    client.print("Hello, world!");
+
+                    client.println();
+                    break;
+                }
+
+                last = c;
+            }
+        }
+        
+        client.stop();
+    }
 }
