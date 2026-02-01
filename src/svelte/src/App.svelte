4121a782bd3fc55798fcf2e8f9f9f9ee075ba216
Ben Beckford <ben@benjaminbeckford.com>
2026-02-01 12:13:53 -0800

    Implement web controller

---
+++ src/svelte/src/App.svelte
@@ -0,0 +1,149 @@
+<script lang="ts">
+    import {
+        draggable,
+        axis,
+        bounds,
+        BoundsFrom,
+        events,
+        Compartment,
+        position,
+        type DragEventData,
+    } from "@neodrag/svelte";
+    import { Tween } from "svelte/motion";
+    import { expoOut } from "svelte/easing";
+
+    const socket = new WebSocket("ws://arduino.local:81");
+
+    const posLeft = new Tween(
+        { x: 0, y: 205 },
+        { easing: expoOut, duration: 1200 },
+    );
+
+    const leftCompartment = Compartment.of(() =>
+        position({ current: $state.snapshot(posLeft.current) }),
+    );
+
+    const posRight = new Tween(
+        { x: 0, y: 205 },
+        { easing: expoOut, duration: 1200 },
+    );
+
+    const rightCompartment = Compartment.of(() =>
+        position({ current: $state.snapshot(posRight.current) }),
+    );
+
+    const dragLeft = (e: DragEventData) => {
+        posLeft.set({ x: e.offset.x, y: e.offset.y }, { duration: 0 });
+
+        socket.send(
+            new Uint8Array([
+                e.offset.y >= 205 ? 0xf : 0,
+                Math.round(
+                    (e.offset.y >= 205
+                        ? e.offset.y - 205
+                        : (205 - e.offset.y) / 205) * 255,
+                ),
+            ]).buffer,
+        );
+    };
+
+    const dragRight = (e: DragEventData) => {
+        posRight.set({ x: e.offset.x, y: e.offset.y }, { duration: 0 });
+        socket.send(
+            new Uint8Array([
+                e.offset.y >= 205 ? 0xff : 0xf0,
+                Math.round(
+                    (e.offset.y >= 205
+                        ? e.offset.y - 205
+                        : (205 - e.offset.y) / 205) * 255,
+                ),
+            ]).buffer,
+        );
+    };
+
+    const endDragLeft = (_) => {
+        posLeft.target = { x: 0, y: 205 };
+        socket.send(new Uint8Array([0, 0]).buffer);
+    };
+
+    const endDragRight = (_) => {
+        posRight.target = { x: 0, y: 205 };
+        socket.send(new Uint8Array([0xf0, 0]).buffer);
+    };
+</script>
+
+<main>
+    <div class="column">
+        <h1>L</h1>
+        <div class="slider">
+            <div
+                {@attach draggable(() => [
+                    leftCompartment,
+                    bounds(BoundsFrom.parent()),
+                    axis("y"),
+                    events({
+                        onDrag: dragLeft,
+                        onDragEnd: endDragLeft,
+                    }),
+                ])}
+                class="grip"
+            ></div>
+        </div>
+    </div>
+    <div class="column">
+        <h1>R</h1>
+        <div class="slider">
+            <div
+                {@attach draggable(() => [
+                    rightCompartment,
+                    bounds(BoundsFrom.parent()),
+                    axis("y"),
+                    events({
+                        onDrag: dragRight,
+                        onDragEnd: endDragRight,
+                    }),
+                ])}
+                class="grip"
+            ></div>
+        </div>
+    </div>
+</main>
+
+<style>
+    main {
+        width: 100%;
+        height: 100%;
+        display: flex;
+    }
+
+    .column {
+        flex: 1;
+        height: 100%;
+        background-color: black;
+    }
+
+    h1 {
+        font-family: sans-serif;
+        font-size: 96pt;
+        text-align: center;
+        color: white;
+    }
+
+    .slider {
+        height: 500px;
+        width: 100px;
+        border-radius: 50px;
+        background-color: #a7a7a7;
+        overflow: hidden;
+        margin: auto;
+        padding: 0 5px;
+    }
+
+    .grip {
+        width: 90px;
+        height: 90px;
+        border-radius: 45px;
+        background-color: blue;
+        position: relative;
+    }
+</style>
