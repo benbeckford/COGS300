c64dc07b0e65ab02459817a0d6e02f9fefac6dc8
RickyyDaii <rickyjdai2005@gmail.com>
2026-02-27 13:33:38 -0800

    Update followline.h

--- src/arduino/followline.h
+++ src/arduino/followline.h
@@ -1,1 +1,17 @@
+#ifndef FOLLOWLINE_H
+#define FOLLOWLINE_H
 
+#include "mode.h"
+
+class FollowLine : public Mode {
+  public:
+  FollowLine();
+  virtual void loop() override;
+  virtual void event(uint8_t * data, size_t len) override;
+  private:
+  bool running;
+  int baseSpeed;
+  int correction;
+};
+
+#endif
