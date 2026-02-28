28d40273748fa419d63e7890b3dd153e5ec31d95
ETH4N0L <144849923+ETH4N0L@users.noreply.github.com>
2026-02-27 18:35:19 -0800

    Update followline.cpp

--- src/arduino/followline.cpp
+++ src/arduino/followline.cpp
@@ -1,46 +1,102 @@
-#include "followline.h"
-#include <Arduino.h>
-#include "pins.h"
-#include "standby.h"
-#include "calibration.h"
+// ---- Pin Definitions ----
+#define LEFT_MOTOR_POWER 6
+#define LEFT_MOTOR_FORWARD 5
+#define LEFT_MOTOR_BACKWARD 7
 
-FollowLine::FollowLine() {
-  Standby();
-  running = false;
-  baseSpeed = 150;
-  correction = 60;
+#define RIGHT_MOTOR_POWER 3
+#define RIGHT_MOTOR_FORWARD 4
+#define RIGHT_MOTOR_BACKWARD 2
+
+#define LEFT_INFRARED   A4
+#define RIGHT_INFRARED  A5
+#define MIDDLE_INFRARED A3
+
+int baseSpeed = 70;
+int correction = 40;
+
+void stopMotors() {
+  analogWrite(LEFT_MOTOR_POWER, 0);
+  analogWrite(RIGHT_MOTOR_POWER, 0);
+
+  digitalWrite(LEFT_MOTOR_FORWARD, LOW);
+  digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
+  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
 }
 
-void FollowLine::loop() {
-  if (!running) return;
+void forward(int leftSpeed, int rightSpeed) {
+  leftSpeed = constrain(leftSpeed, 0, 255);
+  rightSpeed = constrain(rightSpeed, 0, 255);
 
-  int leftSensor = digitalRead(LEFT_INFRARED);
-  int rightSensor = digitalRead(RIGHT_INFRARED);
+  // Direction forward
+  digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
+  digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
+  digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
+  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
 
-  if (leftSensor == LOW && rightSensor == LOW) {
-    analogWrite(LEFT_MOTOR_FORWARD, baseSpeed);
-    analogWrite(RIGHT_MOTOR_FORWARD, baseSpeed);
-  }
-  else if (leftSensor == LOW && rightSensor == HIGH) {
-    analogWrite(LEFT_MOTOR_FORWARD, baseSpeed - correction);
-    analogWrite(RIGHT_MOTOR_FORWARD, baseSpeed + correction);
-  }
-  else if (leftSensor == HIGH && rightSensor == LOW) {
-    analogWrite(LEFT_MOTOR_FORWARD, baseSpeed + correction);
-    analogWrite(RIGHT_MOTOR_FORWARD, baseSpeed - correction);
-  }
+  // Speed control
+  analogWrite(LEFT_MOTOR_POWER, leftSpeed);
+  analogWrite(RIGHT_MOTOR_POWER, rightSpeed);
+}
 
+void backward(int leftSpeed, int rightSpeed) {
+  leftSpeed = constrain (leftSpeed, 0, 255);
+  rightSpeed = constrain (rightSpeed, 0, 255);
+
+  digitalWrite(LEFT_MOTOR_FORWARD, LOW);
+  digitalWrite(LEFT_MOTOR_BACKWARD, HIGH);
+  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD, HIGH);
+
+  analogWrite(LEFT_MOTOR_POWER, leftSpeed);
+  analogWrite(RIGHT_MOTOR_POWER, rightSpeed);
+}
+
+void setup() {
+  pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
+  pinMode(LEFT_MOTOR_BACKWARD, OUTPUT);
+  pinMode(LEFT_MOTOR_POWER, OUTPUT);
+
+  pinMode(RIGHT_MOTOR_FORWARD, OUTPUT);
+  pinMode(RIGHT_MOTOR_BACKWARD, OUTPUT);
+  pinMode(RIGHT_MOTOR_POWER, OUTPUT);
+
+  // Use pullups so the pins don't float
+  pinMode(LEFT_INFRARED, INPUT_PULLUP);
+  pinMode(MIDDLE_INFRARED, INPUT_PULLUP);
+  pinMode(RIGHT_INFRARED, INPUT_PULLUP);
+
+  stopMotors();
+}
+
+void loop() {
+  int leftSensor   = digitalRead(LEFT_INFRARED);
+  int middleSensor = digitalRead(MIDDLE_INFRARED);
+  int rightSensor  = digitalRead(RIGHT_INFRARED);
+
+  // LOW = tape detected, HIGH = no tape
+  bool leftOnTape   = (leftSensor == LOW);
+  bool middleOnTape = (middleSensor == LOW);
+  bool rightOnTape  = (rightSensor == LOW);
+
+  if (!leftOnTape && !middleOnTape && !rightOnTape) {
+    backward(baseSpeed, baseSpeed);
+  }
   else {
-    Standby();
+    // Steering priority: edges first
+    if (leftOnTape && !rightOnTape) {
+      // tape under left -> steer left
+      forward(baseSpeed - correction, baseSpeed + correction);
+    }
+    else if (rightOnTape && !leftOnTape) {
+      // tape under right -> steer right
+      forward(baseSpeed + correction, baseSpeed - correction);
+    }
+    else {
+      // middle only OR multiple sensors see tape -> go straight
+      forward(baseSpeed, baseSpeed);
+    }
   }
-}
 
-void FollowLine::event(uint8_t * data, size_t len) {
-  if (*data == 0x00) {
-    analogWrite(LEFT_MOTOR_POWER, 0);
-    analogWrite(RIGHT_MOTOR_POWER, 0);
-    running = false;
-  } else if (*data & 0x80) {
-    running = true;
-  }
+  delay(10);
 }
